921b72fc9986e5bba97897127b392ebed91c07ba
Rafael Megda <rafaelmisidoro@gmail.com>
2019-10-09 20:18:33 -0300

    Add Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Aula_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 435);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ler";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(305, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(241, 435);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Enabled = false;
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(22, 500);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(240, 231);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea6);
+            legend6.Enabled = false;
+            legend6.Name = "Legend1";
+            this.chart2.Legends.Add(legend6);
+            this.chart2.Location = new System.Drawing.Point(305, 500);
+            this.chart2.Name = "chart2";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(240, 231);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(600, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(241, 435);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // chart3
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea7);
+            legend7.Enabled = false;
+            legend7.Name = "Legend1";
+            this.chart3.Legends.Add(legend7);
+            this.chart3.Location = new System.Drawing.Point(601, 500);
+            this.chart3.Name = "chart3";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart3.Series.Add(series7);
+            this.chart3.Size = new System.Drawing.Size(240, 231);
+            this.chart3.TabIndex = 8;
+            this.chart3.Text = "chart3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 743);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Criando Histogramas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DIPLi;
+
+namespace Aula_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double[] calcularHistograma(DIPLi.Imagem IMG)
+        {
+            double[] valores = new double[256];
+            int niveldecinza;
+
+            for (int i = 0; i < IMG.Altura; i++)
+            {
+                for (int j = 0; j < IMG.Largura; j++)
+                {
+                    niveldecinza = (int)IMG[i, j];
+                    valores[niveldecinza] = valores[niveldecinza] + 1;
+                }
+            }
+            return valores;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Imagem I = new Imagem("E:\\LennaCinza2.jpg");
+            I = I.ToGrayscale();
+            pictureBox1.Image = I.ToBitmap();
+
+            double[] y = new double[256];
+            y = calcularHistograma(I);
+
+            string[] x = new string[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                x[i] = i.ToString();
+            }
+
+            chart1.Series["Series1"].Points.DataBindXY(x, y);
+
+            //y = a * X + b;
+
+            double Xmin = 255;
+            double Xmax = 0;
+            for (int i = 0; i < I.Largura; i++)
+            {
+                for (int j = 0; j < I.Altura; j++)
+                {
+                    try
+                    {
+                        if (I[i, j] < Xmin)
+                        {
+                            Xmin = I[i, j];
+                        }
+
+                        if (I[i, j] > Xmax)
+                        {
+                            Xmax = I[i, j];
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+
+                }
+            }
+
+
+            double a = 255 / (Xmax - Xmin);
+            double b = -a * Xmin;
+
+            for (int i = 0; i < I.Largura; i++)
+            {
+                for (int j = 0; j < I.Altura; j++)
+                {
+                    try
+                    {
+                        I[i, j] = a * I[i, j] + b;
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+            }
+
+            y = new double[256];
+            y = calcularHistograma(I);
+
+            x = new string[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                x[i] = i.ToString();
+            }
+
+            chart2.Series["Series1"].Points.DataBindXY(x, y);
+            pictureBox2.Image = I.ToBitmap();
+
+
+            I = new Imagem("E:\\LennaCinza2.jpg");
+            I = I.ToGrayscale();
+            double[] hist = new double[256];
+            hist = calcularHistograma(I);
+
+            for (int i = 0; i < 256; i++)
+            {
+                hist[i] = hist[i] / (I.Largura * I.Altura);
+            }
+
+            for (int i = 1; i < 256; i++)
+            {
+                hist[i] = hist[i] + hist[i - 1];
+            }
+
+            for (int i = 1; i < 256; i++)
+            {
+                hist[i] = hist[i] * 255;
+            }
+
+            for (int i = 0; i < I.Altura; i++)
+            {
+                for (int j = 0; j < I.Largura; j++)
+                {
+                    double valorantes = I[i, j];
+                    double valordepois = hist[(int)valorantes];
+                    I[i, j] = valordepois;
+                }
+            }
+            y = new double[256];
+            y = calcularHistograma(I);
+            pictureBox3.Image = I.ToBitmap();
+            chart3.Series["Series1"].Points.DataBindXY(x, y);
+        }
+    }
+
+
+}
